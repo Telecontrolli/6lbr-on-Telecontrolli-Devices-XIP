b36d8d4d9750f409ffdc5968875290d7d6858a7f
Mario Castaldo <mario.castaldo@telecontrolli.com>
2017-02-03 14:06:38 +0100

    Add files via upload

---
+++ cc26xx/cc26xx-web-demo/test_led2.h
@@ -0,0 +1,16 @@
+/*
+ * Author: Telecontrolli srl
+ * File: project-conf.h V1.0
+ * Description: The file project-conf.h define the various components and set initial conditions
+ */
+
+ //#ifndef TEST_LED2_H_
+ #define TEST_LED2_H_
+ 
+ /*--------------------------------------------------------*/
+ #include <stdint.h>
+ /*--------------------------------------------------------*/
+ PROCESS_NAME(blink_process);
+ /*--------------------------------------------------------*/
+ //endif /* TEST_LED2_H_ */
+ /*--------------------------------------------------------*/
