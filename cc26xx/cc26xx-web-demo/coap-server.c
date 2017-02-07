cded114dbd9021ff3ef07e54315828023e4b05c1
Mario Castaldo <mario.castaldo@telecontrolli.com>
2017-02-07 08:39:07 +0100

    Update coap-server.c

--- cc26xx/cc26xx-web-demo/coap-server.c
+++ cc26xx/cc26xx-web-demo/coap-server.c
@@ -1,6 +1,6 @@
 /*
  * Author: Telecontrolli srl
- * File: coap-server.c V1.0
+ * File: coap-server.c V1.1
  * Description: The file coap-server.c built the list of resources.
  */
 /*---------------------------------------------------------------------------*/
