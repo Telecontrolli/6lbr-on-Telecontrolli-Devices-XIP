4abf85aae72e75f4e28a9e618ed4a075af3e8ab9
Mario Castaldo <mario.castaldo@telecontrolli.com>
2017-02-07 09:33:36 +0100

    Update button-sensor.c

--- srf06-cc26xx/srf06/button-sensor.c
+++ srf06-cc26xx/srf06/button-sensor.c
@@ -1,6 +1,6 @@
 /*
  * Author: Telecontrolli srl
- * File: button-sensor.c V1.0
+ * File: button-sensor.c V1.2
  * Description: Driver for the SmartRF06EB buttons when a CC13xx/CC26xxEM is mounted on it
  */
 /*---------------------------------------------------------------------------*/
