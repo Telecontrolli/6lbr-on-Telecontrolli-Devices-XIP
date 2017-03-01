0520ca04cdf478d90b3a59b0fe1b239e301f6a9c
Mario Castaldo <mario.castaldo@telecontrolli.com>
2017-03-01 11:08:25 +0100

    Update httpd-simple.c

--- cc26xx/cc26xx-web-demo/httpd-simple.c
+++ cc26xx/cc26xx-web-demo/httpd-simple.c
@@ -1,6 +1,6 @@
 /*
  * Author: Telecontrolli srl
- * File: httpd-simple.c V1.0
+ * File: httpd-simple.c V1.3
  * Description: A simple web server which displays network and sensor information
  */
 /*---------------------------------------------------------------------------*/
