f59e71c76b7ab8cdc05ad0c6fa2490144ef5ca23
Mario Castaldo <mario.castaldo@telecontrolli.com>
2017-03-01 11:10:46 +0100

    Update res-device.c

--- cc26xx/cc26xx-web-demo/resources/res-device.c
+++ cc26xx/cc26xx-web-demo/resources/res-device.c
@@ -1,6 +1,6 @@
 /*
  * Author: Telecontrolli srl
- * File: res-device.c V1.0
+ * File: res-device.c V1.3
  * Description: CoAP resource handler for CC26XX software and hardware version
 /*---------------------------------------------------------------------------*/
 #include "contiki.h"
