7aef77d134e6accceed432a36bef7f4c6e5f3b7d
Mario Castaldo <mario.castaldo@telecontrolli.com>
2017-03-31 09:26:50 +0200

    Update res-net.c

--- cc26xx/cc26xx-web-demo/resources/res-net.c
+++ cc26xx/cc26xx-web-demo/resources/res-net.c
@@ -1,6 +1,6 @@
 /*
  * Author: Telecontrolli srl
- * File: res-net.c V1.0
+ * File: res-net.c V1.4
  * Description: CoAP resource handler for network-related resources
  */
 /*---------------------------------------------------------------------------*/
